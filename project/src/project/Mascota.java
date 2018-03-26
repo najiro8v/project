8d7ba23cfbb6cedafa919837bf7fa3711eb316e0
najiro8v <haroldantonio77@outlook.com>
2018-03-26 13:34:45 -0600

    aqui ya se encuentra la calse dueño, mascota , registro, y login

--- project/src/project/Mascota.java
+++ project/src/project/Mascota.java
@@ -10,5 +10,60 @@ package project;
  * @author reyna
  */
 public class Mascota {
-    
+    private String Name,Sexo;
+    private int Edad;
+    private float Peso,Estatura;
+    public Mascota(String name,String sexo,float peso,float estatura,int edad)
+    {
+        this.Edad=edad;
+        this.Estatura=estatura;
+        this.Name=name;
+        this.Peso=peso;
+        this.Sexo=sexo;
+    }
+    public int getEdad()
+    {
+        return Edad;
+    }
+     public String getName()
+    {
+        return Name;
+    }
+      public float getEstatura()
+    {
+        return Estatura;
+    }
+       public float getPeso()
+    {
+        return Peso; 
+    }
+      public String getSexo()
+    {
+        return Sexo; 
+    }
+     /******************************SETs*********************************/
+       public void setEdad(int edad)
+    {
+        this.Edad=edad;
+    }
+     public  void setName(String name)
+    {
+        this.Name=name;
+    }
+      public void setEstatura(float estatura)
+    {
+        this.Estatura=estatura;
+    }
+       public  void setPeso(float peso)
+    {
+        this.Peso=peso; 
+    }
+      public  void setSexo(String sexo)
+    {
+         this.Sexo=sexo; 
+    }
+      public int GetTamañoM()
+      {
+          return getSexo().length()*2+4+4+4+getName().length()*2;
+      }
 }
